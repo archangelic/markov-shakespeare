87830463f5a4ff982827d553b95618211c80b3ce
Mallory Hancock <malloryhancock89@gmail.com>
2017-08-31 08:56:15 -0700

    initial commit

---
+++ markov_shakespeare.py
@@ -0,0 +1,25 @@
+import json
+
+import ananas
+import markovify
+
+class POSifiedText(markovify.Text):
+    def word_split(self, sentence):
+        words = re.split(self.word_split_pattern, sentence)
+        words = [w for w in words if len(w) > 0]
+        words = ["::".join(tag) for tag in nltk.pos_tag(words)]
+        return words
+
+    def word_join(self, words):
+        sentence = " ".join(word.split("::")[0] for word in words)
+        return sentence
+
+class Shakey(ananas.PineappleBot):
+    def start(self):
+        with open('shakespeare.json') as f:
+            text = json.load(f)
+        self.text_model = POSifiedText.from_json(text)
+
+    @ananas.schedule(hour="*/3", minute="32")
+    def post_markov_sentence(self):
+        self.mastodon.toot(self.text_model.make_sentence())
